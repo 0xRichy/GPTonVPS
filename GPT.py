a97deb54efe1556700edf72da93eb9469ba3a4c8
0xRichy <35074751+0xRichy@users.noreply.github.com>
2023-07-27 19:36:29 +0200

    Update GPT.py

--- GPT.py
+++ GPT.py
@@ -3,8 +3,10 @@ import openai
 from flask import Flask, request, jsonify
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from requests import post
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 # Informations de connexion au VPS
 hostname = 'adresse_ip_ou_nom_de_domaine'
@@ -61,12 +63,15 @@ def generate_responses(prompt):
 
     return responses
 
-@app.route('/command', methods=['POST'])
-def receive_command():
-    command = request.json['command']
-    record_command(command)
-    output, error = execute_command(command)
-    return jsonify({'output': output, 'error': error})
+class CommandResource(Resource):
+    def post(self):
+        data = request.get_json()
+        command = data['command']
+        record_command(command)
+        output, error = execute_command(command)
+        return jsonify({'output': output, 'error': error})
+
+api.add_resource(CommandResource, '/command')
 
 @app.route('/telegram', methods=['POST'])
 def receive_telegram_command():
@@ -79,6 +84,15 @@ def receive_telegram_command():
 def chat_with_gpt():
     user_input = request.json['input']
     responses = generate_responses(user_input)
+
+    # Execute commands on VPS
+    for response in responses:
+        url = 'http://adresse_ip_ou_nom_de_domaine:8000/command'  # Replace with your VPS IP or domain
+        data = {'command': response}
+        response = post(url, json=data)
+        result = response.json()
+        # You can handle the output and error if needed
+
     return jsonify({'responses': responses})
 
 # Intégration avec Telegram
